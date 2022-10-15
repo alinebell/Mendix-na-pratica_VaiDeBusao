f312e04ca9f8b244e9cbe2efde78c1191e00af85
Aline Beltrao <alinebelltrao@gmail.com>
2022-10-15 14:59:24 -0300

    arquivando projeto

---
+++ VaiDeBusao_2-main_5/deployment/modern-web/start.bat
@@ -0,0 +1,1 @@
+"C:/Program Files/Mendix/9.12.0.41679\modeler\tools\node\node.exe" "C:/Program Files/Mendix/9.12.0.41679\modeler\tools\node\node_modules\rollup\dist\bin\rollup" -c -w
---
+++ VaiDeBusao_2-main_5/deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "C:/Users/Aline/Documents/Mendix/VaiDeBusao_2-main_5/themesource/administration/native/main";
+import * as style1 from "C:/Users/Aline/Documents/Mendix/VaiDeBusao_2-main_5/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/Aline/Documents/Mendix/VaiDeBusao_2-main_5/themesource/webactions/native/main";
+import * as style3 from "C:/Users/Aline/Documents/Mendix/VaiDeBusao_2-main_5/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/Aline/Documents/Mendix/VaiDeBusao_2-main_5/themesource/myfirstmodule/native/main";
+import * as style5 from "C:/Users/Aline/Documents/Mendix/VaiDeBusao_2-main_5/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
